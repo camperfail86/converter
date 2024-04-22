bf77faf2771bd388d15a0473676424b5db9bd2b7
camperfail86 <mega.kochkin@inbox.ru>
2024-04-23 01:11:07 +0500

    fix 1 task

--- task_1/index.js
+++ task_1/index.js
@@ -1,9 +1,9 @@
 import {encoded, translations} from './data.js'
-
 const decode = (encoded, translations) => {
-	const result = {}
-	for (var index in encoded) {
-		for (var key in encoded[index]) {
+	let array = []
+	for (let index in encoded) {
+		const result = {}
+		for (let key in encoded[index]) {
 			if (key === "groupId" || key === "service" || key === "formatSize" || key === "ca") {
 				result[key] = encoded[index][key];
 			} else if (encoded[index][key] === null) {
@@ -12,7 +12,9 @@ const decode = (encoded, translations) => {
 				result[key] = translations[+encoded[index][key]]
 			}
 		}
+		array.push(result)
 	}
-	return result
+	return array
 }
+
 console.log(decode(encoded, translations))
